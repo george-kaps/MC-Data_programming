42a13ffe7485cfc0998273da83008984c2397847
george-kaps <115156208+george-kaps@users.noreply.github.com>
2023-06-20 11:28:20 +0000

    Initial commit

---
+++ 0x02-python-import_modulea/0-add.py
@@ -0,0 +1,14 @@
+# Python Module addition
+
+__import__("0-add")
+
+def add(a, b):
+    a = 1
+    b = 2
+
+   result = a + b
+   return result
+  print("result")
+
+if __name__=="__main__":
+ main()
