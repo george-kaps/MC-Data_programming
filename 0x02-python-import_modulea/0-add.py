b9ac7aa8eab69fbcae8f75ae0fa02c06cc49b010
george-kaps <115156208+george-kaps@users.noreply.github.com>
2023-06-21 17:16:32 +0000

    Function commit

--- 0x02-python-import_modulea/0-add.py
+++ 0x02-python-import_modulea/0-add.py
@@ -3,12 +3,12 @@
 __import__("0-add")
 
 def add(a, b):
-    a = 1
-    b = 2
+    a: 1
+    b: 2
 
    result = a + b
    return result
   print("result")
 
-if __name__=="__main__":
+if __name__ == "__main__":
  main()
